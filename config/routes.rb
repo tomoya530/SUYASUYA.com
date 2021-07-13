16ce1a3df9a9f48ca7f02b911a1e3306aa6cecab
tomoya530 <sugimoto.tomoya530@gmail.com>
2021-07-13 15:56:48 +0000

    genre_Finish

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,24 @@
 Rails.application.routes.draw do
+
+  # devise_for :admins, controllers: {
+  # sessions:      'admin/sessions',
+  # passwords:     'admin/passwords',
+  # registrations: 'admin/registrations'
+  # }
+
+  # devise_for :users, controllers: {
+  # sessions:      'public/sessions',
+  # passwords:     'public/passwords',
+  # registrations: 'public/registrations'
+  # }
+
+  devise_for :admins
   devise_for :users
   root to: 'homes#top'
   get "about" => "homes#about"
+
+  namespace :manager do
+    resources :genres
+  end
+
 end
